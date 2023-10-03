83990aee6d3fac3e8191f8879300b1dd0fa16ac4
Manuel Proß <fhs49068@fh-salzburg.ac.at>
2023-10-03 18:27:47 +0200

    feat: make space indenting to four whitespaces

--- lua/manuel/core/options.lua
+++ lua/manuel/core/options.lua
@@ -1,9 +1,11 @@
 local options = {
-	number = true,
-	relativenumber = true,
-	termguicolors = true,
+    number = true,
+    relativenumber = true,
+    termguicolors = true,
+    expandtab = true,
+    shiftwidth = 4
 }
 
 for k, v in pairs(options) do
-	vim.opt[k] = v
+    vim.opt[k] = v
 end
