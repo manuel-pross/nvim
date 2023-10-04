3984e810f7ee7833b2e21ff65d0b75f9a3b28fab
Manuel Proß <manuel.pross@getflip.com>
2023-10-04 12:46:02 +0200

    feat: add keymaps for managing buffers

--- lua/manuel/core/keymaps.lua
+++ lua/manuel/core/keymaps.lua
@@ -9,3 +9,10 @@ vim.g.maplocalleader = " "
 -- netrw
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Map keys using the leader key
+vim.api.nvim_set_keymap('n', '<leader>n', ':bn<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>b', ':bp<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>t', ':enew<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader><leader>', '<C-W>w', opts)
+
+
