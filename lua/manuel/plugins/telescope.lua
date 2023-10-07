8ed47c9fa96a7343e1d31c1363ea95369b3114ad
Manuel Proß <fhs49068@fh-salzburg.ac.at>
2023-10-07 15:28:14 +0200

    fix: remove unnecessary spaces

--- lua/manuel/plugins/telescope.lua
+++ lua/manuel/plugins/telescope.lua
@@ -1,10 +1,10 @@
 return {
-    'nvim-telescope/telescope.nvim', 
-    tag = '0.1.3', 
-    dependencies = { 
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.3',
+    dependencies = {
 	    'nvim-lua/plenary.nvim',
 	    "nvim-tree/nvim-web-devicons",
-    }, 
+    },
     config = function()
 	    local telescope = require("telescope")
 	    local actions = require('telescope.actions')
