8deb3d6c4d0fb3aa9ebb25767febc243eba48ef5
Manuel Proß <fhs49068@fh-salzburg.ac.at>
2023-10-09 07:08:05 +0200

    feat: configure treesitter syntax highlighting and incremental search

---
+++ lua/manuel/plugins/nvim-treesitter.lua
@@ -0,0 +1,46 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local treesitter = require("nvim-treesitter.configs")
+
+        treesitter.setup({
+            highlight = {
+                enable = true,
+            },
+
+            indent = { enable = true },
+
+            -- ensure these language parsers are installed
+            ensure_installed = {
+                "json",
+                "javascript",
+                "typescript",
+                "tsx",
+                "yaml",
+                "html",
+                "scss",
+                "css",
+                "markdown",
+                "markdown_inline",
+                "svelte",
+                "lua",
+                "vim",
+                "dockerfile",
+                "gitignore",
+                "query",
+            },
+
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<C-space>",
+                    node_incremental = "C-space>",
+                    scope_incremental = false,
+                    node_decremental = "<bs>",
+                },
+            },
+        })
+    end,
+}
