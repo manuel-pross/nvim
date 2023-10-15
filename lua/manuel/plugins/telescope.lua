7c0677d0189533332ab9a309f6a5121f54f88633
Manuel Proß <fhs49068@fh-salzburg.ac.at>
2023-10-15 21:32:17 +0200

    feat: add command for git search, remove doubled custom keymaps

--- lua/manuel/plugins/telescope.lua
+++ lua/manuel/plugins/telescope.lua
@@ -21,18 +21,11 @@ return {
 
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
-    local builtin = require("telescope.builtin")
 
     keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-
-    vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-
-    -- find keyword (ripgrep)
-    vim.keymap.set("n", "<leader>ps", function()
-      builtin.grep_string({ search = vim.fn.input("Grep > ") })
-    end)
+    keymap.set("n", "<leader>fg", "<cmd>Telescope git_files<cr>", { desc = "Fuzzy find files tracked by git" })
   end,
 }
